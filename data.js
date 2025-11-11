d104d1b054c7a862ee988c46abc734d85bd027e8
manaka-web <enyukari.a.manaka@gmail.com>
2025-11-11 14:25:13 +0900

    okok

---
+++ data.js
@@ -0,0 +1,109 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "入り口🚪",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.076077216937545,
+          "pitch": 0.3214869924416579,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.606568207248225,
+          "pitch": 0.019762962966925812,
+          "title": "あずまや☔️",
+          "text": "雨でもへっちゃら"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "入ってすぐ🐈",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08671403266077604,
+          "pitch": 0.32998256917329627,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5014289914665344,
+          "pitch": 0.03505409897953449,
+          "title": "ベンチ🪵",
+          "text": "疲れたらひと休み"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
